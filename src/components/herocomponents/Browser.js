bcfb131bd53102dc829d3c39e7893dec8b17f2f0
Fatih <kresnofatihimani.private@gmail.com>
2021-02-07 15:43:44 +0700

    set async rendering :airplane: :airplane:

--- src/components/herocomponents/Browser.js
+++ src/components/herocomponents/Browser.js
@@ -1,14 +1,23 @@
 import React, {useState} from 'react'
 import './Browser.css'
+import Searchitem from './Searchitem';
 
 function Browser() {
     const [searchKeyword, setSearchKeyword] = useState('')
+    const [hasData, setHasData] = useState(false)
+    const [data, setData] = useState([])
     const searchAnime = async (searchKeyword) => {
+        setData([]);
         const keyword = searchKeyword.trim();
-        const resp = await fetch(`https://api.jikan.moe/v3/search/anime?q=${keyword}&page=1`);
-        const data = await resp.json();
-        console.log(data.results);
-        console.log(data.results.length);
+        const resp = await fetch(`https://api.jikan.moe/v3/search/anime?q=${keyword}&limit=5`);
+        const respjson = await resp.json();
+        respjson.results.forEach((item)=>{
+            data.push(item.mal_id)
+        })
+        setHasData(true);
+        data.forEach((item)=>{
+            console.log(item);
+        })
     }
     return (
         <div className="browser">
@@ -19,8 +28,15 @@ function Browser() {
                     placeholder="Enter anime name here..."
                     onChange={(e)=>setSearchKeyword(e.target.value)}
                 />
-                <button onClick={()=>searchAnime(searchKeyword)}>Search</button>
+                <button 
+                    onClick={()=>{
+                        searchAnime(searchKeyword);
+                        setSearchKeyword('');
+                }}>Search</button>
             </div>
+            {hasData &&
+                <p>haha</p>
+            }
         </div>
     )
 }
--- src/components/herocomponents/Searchitem.js
+++ src/components/herocomponents/Searchitem.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Searchitem() {
+    // const [data, setData] = useState('')
+    // const getAnimeData = async () => {
+    //     const resp = await fetch(`https://api.jikan.moe/v3/anime/${id}`);
+    //     const respjson = await resp.json();
+    //     console.log(respjson);
+    //     setData(respjson);
+    // }
+    // getAnimeData();
+    // console.log(id);
     return (
-        <div>
-            
+        <div className="searchitem">
+            <div className="searchiteminfo">
+                <p>haha</p>
+            </div>
         </div>
     )
 }
